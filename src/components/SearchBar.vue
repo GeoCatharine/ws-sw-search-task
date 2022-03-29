10742050e900eaffc6448209887f41db7f807892
GeoCatharine <v.nahornakova@gmail.com>
2022-03-29 23:03:23 +0200

    added loading, red highlight if no match while searching, error msg

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,14 +8,21 @@
         :dense="dense" 
         debounce="1000" 
         color="ws-gem"
+        :loading="isLoading"
         @input="getAllPeople"
         > 
           <template v-slot:append>
-            <q-icon name="search" />
+            <q-icon v-show="!isLoading" name="search" />
           </template> 
+          <template v-slot:default>
+            <span v-bind:class="{ 'highlight': !match, 'hide': match }"> {{ search }} </span>
+          </template>
       </q-input>
       <div v-if="!search">
         <p>Enter search pharse</p>
+      </div>
+      <div v-else-if="showError">
+        <p>Oops, something went wrong.</p>
       </div>
       <div v-else-if="names.length > 0" class="list" >
         <div class="text-white">
@@ -44,29 +51,50 @@ export default {
         return {
             search: "",
             names: [],
-            dense: false
+            dense: false,
+            isLoading: false,
+            showError: false,
         };
     },
     methods: {
       getAllPeople() {
+        this.isLoading = true;
+
+        if (!this.search) {
+          this.isLoading = false;
+          return;
+        }
+
+        if (this.showError) {
+          this.showError = false;
+        }
+
         axios.get("https://swapi.py4e.com/api/people/", { params: { search: this.search } })
           .then((response) => {
             this.names = response.data.results;
-            console.log(this.names.length)
           })
           .catch(err => {
-            console.log("ohh something went wrong");
+            this.showError = true;
+          })
+          .finally( () => {
+            this.isLoading = false;
           });
+      },
+    },
+    computed: {
+      match() {
+        return this.names.length > 0;
       },
     },
     components: { BoldenListItem }
 }
 </script>
-<style scss>
+<style sass>
 
   .container {
     width: 50%;
     margin: 0 auto;
+    position: relative;
   }
 
   .list {
@@ -81,10 +109,27 @@ export default {
   }
 
   .q-input {
+    z-index: 2;
     width: 50%;
     margin: 0 auto;
     border-radius: 5px;
     padding: 20px 0;
+    background-color: transparent;
+    overflow: hidden;
+  }
+
+  .highlight {
+    position: absolute;
+    left: 0;
+    bottom: 10px;
+    background-color: #FEE6E0;
+    color: transparent;
+    z-index: -1;
+    font-size: 15px;
+  }
+
+  .hide {
+    visibility: hidden;
   }
 
   h1 {
